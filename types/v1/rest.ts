b519a1977b3b94d8eb7e5c3088e8f1af5d3f5ea0
almeida <hello@eualmeida.com.br>
2026-02-17 19:11:35 -0300

    docs(v1): add JSDoc in REST types with the respective route

--- types/v1/rest.ts
+++ types/v1/rest.ts
@@ -48,10 +48,13 @@ export type APIResponse<Data> =
 			};
 	  };
 
+/** `GET https://api.rewritetoday.com/v1/projects/:id/webhooks/:webhookId`. */
 export type RESTGetWebhookData = APIResponse<APIWebhook>;
 
+/** `POST https://api.rewritetoday.com/v1/projects/:id/webhooks`. */
 export type RESTPostCreateWebhookData = APIResponse<APIWebhook>;
 
+/** `POST https://api.rewritetoday.com/v1/projects/:id/webhooks`. */
 export interface RESTPostCreateWebhookBody {
 	/** Webhook name. */
 	name: APIWebhook['name'];
@@ -61,10 +64,13 @@ export interface RESTPostCreateWebhookBody {
 	events: APIWebhook['events'];
 }
 
+/** `DELETE https://api.rewritetoday.com/v1/projects/:id/webhooks/:webhookId`. */
 export type RESTDeleteWebhookData = APIResponse<never>;
 
+/** `PATCH https://api.rewritetoday.com/v1/projects/:id/webhooks/:webhookId`. */
 export type RESTPatchUpdateWebhookData = APIResponse<APIWebhook>;
 
+/** `PATCH https://api.rewritetoday.com/v1/projects/:id/webhooks/:webhookId`. */
 export interface RESTPatchUpdateWebhookBody {
 	/** Optional webhook name. */
 	name?: APIWebhook['name'];
@@ -79,16 +85,22 @@ export interface RESTPatchUpdateWebhookBody {
 	status?: WebhookStatus;
 }
 
+/** `GET https://api.rewritetoday.com/v1/projects/:id/webhooks`. */
 export type RESTGetListWebhooksData = APIResponse<APIWebhook[]>;
 
+/** `GET https://api.rewritetoday.com/v1/projects/:id/webhooks`. */
 export type RESTGetListWebhooksQueryParams = RESTCursorOptions;
 
+/** `GET https://api.rewritetoday.com/v1/projects/:id/templates`. */
 export type RESTGetListTemplatesData = APIResponse<APITemplate[]>;
 
+/** `GET https://api.rewritetoday.com/v1/projects/:id/templates`. */
 export type RESTGetListTemplatesQueryParams = RESTCursorOptions;
 
+/** `POST https://api.rewritetoday.com/v1/projects/:id/templates`. */
 export type RESTPostCreateTemplateData = APIResponse<APITemplate>;
 
+/** `POST https://api.rewritetoday.com/v1/projects/:id/templates`. */
 export interface RESTPostCreateTemplateBody {
 	/** Template name. */
 	name: APITemplate['name'];
@@ -97,8 +109,10 @@ export interface RESTPostCreateTemplateBody {
 	variables: APITemplate['variables'];
 }
 
+/** `PATCH https://api.rewritetoday.com/v1/projects/:id/templates/:templateId`. */
 export type RESTPatchUpdateTemplateData = APIResponse<APITemplate>;
 
+/** `PATCH https://api.rewritetoday.com/v1/projects/:id/templates/:templateId`. */
 export interface RESTPatchUpdateTemplateBody {
 	/** Template name. */
 	name: APITemplate['name'];
@@ -107,16 +121,22 @@ export interface RESTPatchUpdateTemplateBody {
 	variables: APITemplate['variables'];
 }
 
+/** `DELETE https://api.rewritetoday.com/v1/projects/:id/templates/:templateId`. */
 export type RESTDeleteTemplateData = APIResponse<never>;
 
+/** `GET https://api.rewritetoday.com/v1/projects/:id/templates/:templateId`. */
 export type RESTGetTemplateData = APIResponse<APITemplate>;
 
+/** `GET https://api.rewritetoday.com/v1/projects/:id/api-keys`. */
 export type RESTGetListAPIKeysData = APIResponse<APIAPIKey[]>;
 
+/** `GET https://api.rewritetoday.com/v1/projects/:id/api-keys`. */
 export type RESTGetListAPIKeysQueryParams = RESTCursorOptions;
 
+/** `POST https://api.rewritetoday.com/v1/projects/:id/api-keys`. */
 export type RESTPostCreateAPIKeyData = APIResponse<APIAPIKey>;
 
+/** `POST https://api.rewritetoday.com/v1/projects/:id/api-keys`. */
 export interface RESTPostCreateAPIKeyBody {
 	/** API key name. */
 	name: APIAPIKey['name'];
@@ -125,17 +145,22 @@ export interface RESTPostCreateAPIKeyBody {
 	scopes: APIAPIKey['scopes'];
 }
 
+/** `DELETE https://api.rewritetoday.com/v1/projects/:id/api-keys/:apiKeyId`. */
 export type RESTDeleteAPIKeyData = APIResponse<never>;
 
+/** `POST https://api.rewritetoday.com/v1/projects`. */
 export type RESTPostCreateProjectData = APIResponse<APIProject>;
 
+/** `POST https://api.rewritetoday.com/v1/projects`. */
 export interface RESTPostCreateProjectBody {
 	/** Project name. */
 	name: APIProject['name'];
 }
 
+/** `PATCH https://api.rewritetoday.com/v1/projects/:id`. */
 export type RESTPatchUpdateProjectData = APIResponse<APIProject>;
 
+/** `PATCH https://api.rewritetoday.com/v1/projects/:id`. */
 export interface RESTPatchUpdateProjectBody {
 	/** Optional project name. */
 	name?: APIProject['name'];
@@ -144,6 +169,8 @@ export interface RESTPatchUpdateProjectBody {
 	icon?: null;
 }
 
+/** `DELETE https://api.rewritetoday.com/v1/projects/:id`. */
 export type RESTDeleteProjectData = APIResponse<never>;
 
+/** `GET https://api.rewritetoday.com/v1/projects/:id`. */
 export type RESTGetProjectData = APIResponse<APIProject>;
