a8d9c14bb80442e396e064b652dc2459a4998801
almeida <hello@eualmeida.com.br>
2026-03-20 12:12:38 -0300

    feat(routes): add new v1 routes

--- types/v1/routes.ts
+++ types/v1/routes.ts
@@ -4,80 +4,96 @@ import { createCursorQuery } from './utils';
 export const API_BASE_URL = 'https://api.rewritetoday.com';
 
 export const Routes = {
+	otp: {
+		/** `POST https://api.rewritetoday.com/v1/otp`. */
+		send() {
+			return '/otp';
+		},
+		/** `POST https://api.rewritetoday.com/v1/otp/:id/verify`. */
+		verify(id: string) {
+			return `/otp/${id}/verify`;
+		},
+	},
+	messages: {
+		/** `POST https://api.rewritetoday.com/v1/messages`. */
+		send() {
+			return '/messages';
+		},
+		/** `POST https://api.rewritetoday.com/v1/messages/batch`. */
+		batch() {
+			return '/messages/batch';
+		},
+		/** `GET https://api.rewritetoday.com/v1/messages`. */
+		list() {
+			return '/messages';
+		},
+		/** `GET https://api.rewritetoday.com/v1/messages/:id`. */
+		get(id: string) {
+			return `/messages/:${id}`;
+		},
+		/** `POST https://api.rewritetoday.com/v1/messages/:id/cancel`. */
+		cancel(id: string) {
+			return `/messages/${id}/cancel`;
+		},
+	},
 	webhooks: {
-		/** `GET https://api.rewritetoday.com/v1/projects/:id/webhooks`. */
-		list(id: string, options?: RESTCursorOptions) {
-			return `/projects/${id}/webhooks?${createCursorQuery(options)}`;
+		/** `GET https://api.rewritetoday.com/v1/webhooks`. */
+		list(options?: RESTCursorOptions) {
+			return `/webhooks?${createCursorQuery(options)}`;
 		},
-		/** `POST https://api.rewritetoday.com/v1/projects/:id/webhooks`. */
-		create(id: string) {
-			return `/projects/${id}/webhooks`;
+		/** `POST https://api.rewritetoday.com/v1/webhooks`. */
+		create() {
+			return '/webhooks';
 		},
-		/** `PATCH https://api.rewritetoday.com/v1/projects/:id/webhooks/:webhookId`. */
-		update(id: string, webhookId: string) {
-			return `/projects/${id}/webhooks/${webhookId}`;
+		/** `PATCH https://api.rewritetoday.com/v1/webhooks/:id`. */
+		update(id: string) {
+			return `/webhooks/${id}`;
 		},
-		/** `DELETE https://api.rewritetoday.com/v1/projects/:id/webhooks/:webhookId`. */
-		delete(id: string, webhookId: string) {
-			return `/projects/${id}/webhooks/${webhookId}`;
+		/** `DELETE https://api.rewritetoday.com/v1/webhooks/:id`. */
+		delete(id: string) {
+			return `/webhooks/${id}`;
 		},
-		/** `GET https://api.rewritetoday.com/v1/projects/:id/webhooks/:webhookId`. */
-		get(id: string, webhookId: string) {
-			return `/projects/${id}/webhooks/${webhookId}`;
+		/** `GET https://api.rewritetoday.com/v1/webhooks/:id`. */
+		get(id: string) {
+			return `/webhooks/${id}`;
+		},
+		/** `GET https://api.rewritetoday.com/v1/webhooks/:id/logs`. */
+		logs(id: string) {
+			return `/webhooks/${id}/logs`;
 		},
 	},
 	templates: {
-		/** `GET https://api.rewritetoday.com/v1/projects/:id/templates`. */
-		list(id: string, options?: RESTCursorOptions) {
-			return `/projects/${id}/templates?${createCursorQuery(options)}`;
+		/** `GET https://api.rewritetoday.com/v1/templates`. */
+		list(options?: RESTCursorOptions) {
+			return `/templates?${createCursorQuery(options)}`;
 		},
-		/** `POST https://api.rewritetoday.com/v1/projects/:id/templates`. */
-		create(id: string) {
-			return `/projects/${id}/templates`;
+		/** `POST https://api.rewritetoday.com/v1/templates`. */
+		create() {
+			return '/templates';
 		},
-		/** `PATCH https://api.rewritetoday.com/v1/projects/:id/templates/:templateId`. */
-		update(id: string, templateId: string) {
-			return `/projects/${id}/templates/${templateId}`;
+		/** `PATCH https://api.rewritetoday.com/v1/templates/:id`. */
+		update(id: string) {
+			return `/templates/${id}`;
 		},
-		/** `DELETE https://api.rewritetoday.com/v1/projects/:id/templates/:templateId`. */
-		delete(id: string, templateId: string) {
-			return `/projects/${id}/templates/${templateId}`;
+		/** `DELETE https://api.rewritetoday.com/v1/projects/:id/templates/:id`. */
+		delete(id: string) {
+			return `/templates/${id}`;
 		},
-		/** `GET https://api.rewritetoday.com/v1/projects/:id/templates/:templateId`. */
-		get(id: string, templateId: string) {
-			return `/projects/${id}/templates/${templateId}`;
+		/** `GET https://api.rewritetoday.com/v1/projects/:id/templates/:id`. */
+		get(id: string) {
+			return `/templates/${id}`;
 		},
 	},
 	apiKeys: {
-		/** `GET https://api.rewritetoday.com/v1/projects/:id/api-keys`. */
-		list(id: string, options?: RESTCursorOptions) {
-			return `/projects/${id}/api-keys?${createCursorQuery(options)}`;
-		},
-		/** `POST https://api.rewritetoday.com/v1/projects/:id/api-keys`. */
-		create(id: string) {
-			return `/projects/${id}/api-keys`;
-		},
-		/** `DELETE https://api.rewritetoday.com/v1/projects/:id/api-keys/:apiKeyId`. */
-		delete(id: string, apiKeyId: string) {
-			return `/projects/${id}/api-keys/${apiKeyId}`;
+		/** `DELETE https://api.rewritetoday.com/v1/api-keys/:key`. */
+		delete(id: string) {
+			return `/api-keys/${id}`;
 		},
 	},
-	projects: {
-		/** `POST https://api.rewritetoday.com/v1/projects`. */
-		create() {
-			return `/projects`;
-		},
-		/** `PATCH https://api.rewritetoday.com/v1/projects/:id`. */
-		update(id: string) {
-			return `/projects/${id}`;
-		},
-		/** `DELETE https://api.rewritetoday.com/v1/projects/:id`. */
-		delete(id: string) {
-			return `/projects/${id}`;
-		},
-		/** `GET https://api.rewritetoday.com/v1/projects/:id`. */
+	logs: {
+		/** `GET /logs/:id`. */
 		get(id: string) {
-			return `/projects/${id}`;
+			return `/logs/${id}`;
 		},
 	},
 };
